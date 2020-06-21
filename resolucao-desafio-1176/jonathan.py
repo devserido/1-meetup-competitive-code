2f8915f8b580a27e7cac0052b9b58c05afbe16df
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-06-21 14:35:06 -0300

    solucao em python

--- resolucao-desafio-1176/jonathan.py
+++ resolucao-desafio-1176/jonathan.py
@@ -2,18 +2,14 @@ from functools import lru_cache
 
 @lru_cache(maxsize=64)
 def fib(t):
-    if t == 0:
-        return 0
-    elif t == 1:
-        return 1
+    if t == 0 or t == 1:
+        return t
     return fib(t-1) + fib(t-2)
 
 n = int(input())
 
-for i in range(n):
+for _ in range(n):
     t = int(input())
     if (0 <= t <= 60):
-        print("Fib({}) = {}".format(
-            t,
-            fib(t)
-        ))
+        print("Fib({}) = {}".format(t, fib(t)))
+        
